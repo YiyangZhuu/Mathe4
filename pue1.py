8a15ac1fcde7fcb6a8f18075a933060bd5e54c08
YiyangZhuu <yiyang.zhu@rwth-aachen.de>
2024-05-04 10:59:05 +0200

    framwork

---
+++ pue1.py
@@ -0,0 +1,31 @@
+# 1.Programmieruebung #
+# 411927 Nikoly Panov; 429943 Yanliang Zhu; 442567 Shijie Xu; 447931 Yiyang Zhu
+
+import numpy as np
+import matplotlib as plt
+
+pi = np.pi
+
+def DCT(F):
+    return D
+
+def TDCT(D,x,y):
+    return A
+
+# Aufgabe 1
+# Testfunktion (a)
+def f1(x,y):
+    return np.cos(2*x) + np.cos(3*y)
+
+# Testfunktion (b)
+def f2(x,y):
+    return (x-pi/2)*(x-pi/2) + (y-pi/2)*(y-pi/2)
+
+def Test():
+    return
+
+# Aufgabe 2
+
+# Aufgabe 3
+def Zickzack():
+    return
